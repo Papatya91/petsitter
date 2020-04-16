0bca8afe86b7407e852277cd35128b4f761bc878
Paula Kusinska <paula.kusinska@gmail.com>
2020-04-16 23:45:23 +0900

    fixed booking card

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,7 @@
 import "bootstrap";
 import "../plugins/flatpickr";
+import JQuery from 'jquery';
+window.$ = window.JQuery = JQuery;
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
+
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -14,4 +14,12 @@ environment.plugins.prepend('Provide',
   })
 )
 
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    'window.jQuery': 'jquery'
+ })
+)
+
 module.exports = environment
